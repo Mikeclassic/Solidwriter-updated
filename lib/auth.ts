20c2ec8602ac1d1ce94e355d2381ad860c64b6e6
Mikeclassic <120394575+Mikeclassic@users.noreply.github.com>
2025-12-01 21:53:35 +0300

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,5 @@
 import { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { db } from "@/lib/db";
 import bcrypt from "bcryptjs";
@@ -8,33 +7,31 @@ import bcrypt from "bcryptjs";
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db),
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-    }),
     CredentialsProvider({
-      name: "Email and Password",
+      name: "credentials",
       credentials: {
-        email: { label: "Email", type: "email" },
+        email: { label: "Email", type: "text" },
         password: { label: "Password", type: "password" }
       },
       async authorize(credentials) {
         if (!credentials?.email || !credentials?.password) {
-          throw new Error("Missing credentials");
+          throw new Error("Invalid credentials");
         }
 
         const user = await db.user.findUnique({
           where: { email: credentials.email }
         });
 
-        // Check if user exists and has a password (if they signed up via Google, they won't have a password)
         if (!user || !user.password) {
           throw new Error("Invalid credentials");
         }
 
-        const isValid = await bcrypt.compare(credentials.password, user.password);
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
 
-        if (!isValid) {
+        if (!isCorrectPassword) {
           throw new Error("Invalid credentials");
         }
 
@@ -44,7 +41,7 @@ export const authOptions: NextAuthOptions = {
   ],
   session: { strategy: "jwt" },
   pages: {
-    signIn: "/auth", // We will build a custom page for this
+    signIn: '/auth',
   },
   callbacks: {
     session: async ({ session, token }) => {
